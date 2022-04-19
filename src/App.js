ae55f7f1bf9aebb890877ccbae1a1f337d218f0c
Minae Lee <minae.lee@gmail.com>
2022-04-18 22:22:38 -0700

    update App component in prep for adding Header component in next exercise

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import './App.css';
 const message = 'cool cud';
 
 const Heart = (props) => {
-  return (
-      <div className='heart'>
-        <img className='heart-img' src={HeartLogo} alt='heart' />
-        <p className='heart-message'>{props.msg}</p>
-      </div>
-  );
-}
+    return (
+        <div className='heart'>
+            <img className='heart-img' src={HeartLogo} alt='heart' />
+            <p className='heart-message'>{props.msg}</p>
+        </div>
+    );
+};
 
 function App() {
-    return <Heart msg={message} />;
+    return (
+        <>
+            <Header />
+            <Heart msg={message} />
+        </>
+    );
 }
 
 export default App;
